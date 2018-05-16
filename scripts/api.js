db83b436e1a357f61ec059e73140e1e7a291f719
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-05-16 12:06:14 -0400

    1st push

--- scripts/api.js
+++ scripts/api.js
@@ -5,6 +5,7 @@ const api = (function(){
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/harley';
   const getItems = function(callback){
     callback('api module works!');
+    $.getJSON(`${BASE_URL}/items`, callback)
   };
   return {
     getItems,
--- scripts/index.js
+++ scripts/index.js
@@ -10,6 +10,7 @@ store.items.push(Item.create('apples'));
 
 api.getItems(function(data) {
   console.log(data);
+  
 });
 
 console.log(api.BASE_URL);
